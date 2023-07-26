5bb68908f90b120f42bed257a0cd94cd6577ee52
Bharat <databharat0007@gmail.com>
2023-07-26 18:47:03 +0530

    frontend backend separated

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
-const path = require("path");
+// const path = require("path");
 
 const dotenv = require("dotenv");
 
@@ -28,11 +28,11 @@ app.use("/api/v1", userRoute);
 app.use("/api/v1", orderRoute);
 app.use("/api/v1", paymentRoute); 
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-})
+// app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+// })
 
 // Middleware for Errors
 app.use(errorMiddleware);
